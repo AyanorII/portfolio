1936fff88be06bab604c8d2d0d96c0b485fb4801
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-06-13 22:19:33 +0900

    fixed skills icons isze

--- components/About/Skills.tsx
+++ components/About/Skills.tsx
@@ -1,6 +1,6 @@
-import { Container, Grid, Stack, Typography } from "@mui/material";
+import { Box, Container, Grid, Stack, Typography } from "@mui/material";
 import Image from "next/image";
-import { PRIMARY_MAIN, PRIMARY_LIGHT, PRIMARY_DARK } from "../../styles/Theme";
+import { PRIMARY_DARK, PRIMARY_LIGHT, PRIMARY_MAIN } from "../../styles/Theme";
 
 type Props = {};
 
@@ -11,85 +11,92 @@ const Skills = (props: Props) => {
       icon: "/icons/html-5.png",
     },
     {
-      name: "React",
-      icon: "/icons/react.png",
-    },
-    {
-      name: "Ruby",
-      icon: "/icons/ruby.png",
-    },
-    {
       name: "CSS",
       icon: "/icons/css-3.png",
-    },
-    {
-      name: "Next.js",
-      icon: "/icons/nextjs.png",
-    },
-    {
-      name: "Ruby on Rails",
-      icon: "/icons/rails.png",
     },
     {
       name: "JavaScript",
       icon: "/icons/js.png",
     },
     {
+      name: "React",
+      icon: "/icons/react.png",
+    },
+    {
+      name: "Next.js",
+      icon: "/icons/nextjs.png",
+    },
+
+    {
       name: "TypeScript",
       icon: "/icons/typescript.png",
+    },
+    {
+      name: "Ruby",
+      icon: "/icons/ruby.png",
+    },
+
+    {
+      name: "Ruby on Rails",
+      icon: "/icons/rails.png",
     },
   ];
 
   return (
     <Container>
-      <Grid container spacing={5}>
-        <Grid item xs={12} marginBottom={2} marginTop={{xs: 5, sm: 10, lg: 15}}>
-          <Typography
-            variant="h3"
-            textAlign="center"
-            color="primary"
-            fontWeight="500"
-            fontFamily="Montserrat"
-            sx={{
-              background: `-webkit-linear-gradient(${PRIMARY_MAIN}, ${PRIMARY_LIGHT})`,
-              WebkitBackgroundClip: "text",
-              WebkitTextFillColor: "transparent",
-              filter: `drop-shadow(0 4px 30px ${PRIMARY_DARK})`,
-            }}
-          >
-            My skills
-          </Typography>
-        </Grid>
-        {skills.map((skill) => {
-          const { name, icon } = skill;
+      <Box
+        marginTop={{ xs: 5, sm: 10, lg: 15 }}
+        marginBottom={{ xs: 5, sm: 7 }}
+      >
+        <Typography
+          variant="h3"
+          textAlign="center"
+          color="primary"
+          fontWeight="500"
+          fontFamily="Montserrat"
+          sx={{
+            background: `-webkit-linear-gradient(${PRIMARY_MAIN}, ${PRIMARY_LIGHT})`,
+            WebkitBackgroundClip: "text",
+            WebkitTextFillColor: "transparent",
+            filter: `drop-shadow(0 4px 30px ${PRIMARY_DARK})`,
+          }}
+        >
+          My skills
+        </Typography>
+      </Box>
+      <Container maxWidth="md">
+        <Grid container spacing={{ xs: 8, sm: 5 }}>
+          {skills.map((skill) => {
+            const { name, icon } = skill;
 
-          return (
-            <Grid item xs={6} sm={4} key={name} alignSelf="center">
-              <Stack
-                flexDirection="row"
-                gap={2}
-                justifyContent={{ xs: "start", md: "center" }}
-                alignItems="center"
-              >
-                <Typography
-                  variant="h5"
-                  fontFamily="Montserrat, sans-serif"
-                  fontWeight="bold"
+            return (
+              <Grid item xs={6} md={4} key={name} alignSelf="center">
+                <Stack
+                  flexDirection={{ xs: "column", sm: "row" }}
+                  gap={2}
+                  justifyContent={{ xs: "start", md: "center" }}
+                  alignItems="center"
                 >
-                  {name}
-                </Typography>
-                <Image
-                  src={icon}
-                  alt={name + " icon"}
-                  width="36px"
-                  height="36px"
-                />
-              </Stack>
-            </Grid>
-          );
-        })}
-        <Grid item xs={6}></Grid>
-      </Grid>
+                  <Typography
+                    variant="h5"
+                    fontFamily="Montserrat, sans-serif"
+                    fontWeight="bold"
+                    whiteSpace="nowrap"
+                  >
+                    {name}
+                  </Typography>
+                  <Image
+                    src={icon}
+                    alt={name + " icon"}
+                    width="36px"
+                    height="36px"
+                  />
+                </Stack>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Container>
     </Container>
   );
 };
