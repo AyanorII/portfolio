7b3fba8b82a600c2d8490d6b42bf2da487a4552f
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-06-17 20:35:56 +0900

    grm-module

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,55 +1,31 @@
 import { ThemeProvider as MuiThemeProvider } from "@mui/material";
 import type { AppProps } from "next/app";
-import { useRouter } from "next/router";
-import Script from "next/script";
 import { useEffect } from "react";
+import TagManager from "react-gtm-module";
 import { ThemeProvider as SCThemeProvider } from "styled-components";
 import Footer from "../components/Footer";
 import Navbar from "../components/Nav/Navbar";
 import ParticlesBackground from "../components/ParticlesBackground";
-import * as gtag from "../lib/gtag";
 import "../styles/globals.css";
 import theme from "../styles/Theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
   useEffect(() => {
-    const handleRouteChange = (url: any) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    router.events.on("hashChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-      router.events.off("hashChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
+    TagManager.initialize({
+      gtmId: "GTM-T55VSZS",
+    });
+  }, []);
 
   return (
-    <>
-      <Script
-        strategy="afterInteractive"
-        id="google-analytics"
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-T55VSZS');`,
-        }}
-      />
-      <MuiThemeProvider theme={theme}>
-        <SCThemeProvider theme={theme}>
-          <>
-            <ParticlesBackground />
-            <Navbar />
-            <Component {...pageProps} />
-            <Footer />
-          </>
-        </SCThemeProvider>
-      </MuiThemeProvider>
-    </>
+    <MuiThemeProvider theme={theme}>
+      <SCThemeProvider theme={theme}>
+        <>
+          <ParticlesBackground />
+          <Navbar />
+          <Component {...pageProps} />
+          <Footer />
+        </>
+      </SCThemeProvider>
+    </MuiThemeProvider>
   );
 }
-
-export default MyApp;
---
+++ pages/_document.tsx
@@ -0,0 +1,23 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5JB47DT"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          />`,
+          }}
+        />
+      </body>
+    </Html>
+  );
+}
