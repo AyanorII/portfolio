780498356a02f96524169457a00bba7e49f64092
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-06-25 15:21:22 +0900

    fixed google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,21 @@ import theme from "../styles/Theme";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload" id="ga">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          page_path: window.location.pathname,
+          });
+        `}
+      </Script>
       <MuiThemeProvider theme={theme}>
         <SCThemeProvider theme={theme}>
           <>
