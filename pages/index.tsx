13f6383a8d35d9a8078e2910eb398c86e9f89d60
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-12-17 09:17:31 +0900

    add dynamic imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,23 @@
 import { useMediaQuery } from "@mui/material";
 import type { NextPage } from "next";
-import AboutSection from "../components/About/AboutSection";
-import Contact from "../components/Contact/Contact";
+import dynamic from "next/dynamic";
+// import AboutSection from "../components/About/AboutSection";
 import Head from "../components/Head";
 import Hero from "../components/Hero/Hero";
-import ProjectsSection from "../components/Projects/ProjectsSection";
 import SocialMediaLinks from "../components/SocialMediaLinks";
 
 const Home: NextPage = () => {
   const isMobile = useMediaQuery("(max-width:899px)");
+
+  const AboutSection = dynamic(
+    () => import("../components/About/AboutSection")
+  );
+
+  const ProjectsSection = dynamic(
+    () => import("../components/Projects/ProjectsSection")
+  );
+
+  const Contact = dynamic(() => import("../components/Contact/Contact"));
 
   return (
     <>
